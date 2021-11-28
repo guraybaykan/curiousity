064011a4713d559f6bad6bd631b4b952f500bfa7
Güray Baykan <gbaykan@thinksmobility.com>
2021-11-28 22:32:57 +0300

    Add project files.

---
+++ Coriousity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coriousity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
